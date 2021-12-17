5b4331f6c465f0de78c3b5dbed8350e7f7450dc0
Visshal Suresh <skvisshal@gmail.com>
2021-12-17 11:13:27 -0500

    guests and gifts

--- visshal_tashi_jbuilder/config/routes.rb
+++ visshal_tashi_jbuilder/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # Your routes here!
+  namespace :api, defaults: { format: :json } do
+    resources :guests, only: [:index, :show] do 
+      resources :gifts, only: [:index]
+    end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :gifts, only: [:show, :index]
+  end
 end
